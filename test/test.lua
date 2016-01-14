55f249a02c2bbf7be48cb89d3af28ed8b65412aa
Michael 'myrhev' Mathieu <michael.mathieu@ens.fr>
2016-01-14 12:26:56 -0500

    Test

--- test/test.lua
+++ test/test.lua
@@ -1,19 +1,33 @@
 require 'thffmpeg'
+require 'sys'
+require 'math'
 
---x = THFFmpeg()
+x = THFFmpeg()
 y = THFFmpeg()
 x = nil
 collectgarbage()
 collectgarbage()
 
-print (y:open('/scratch/datasets/ucf101/UCF-101/ApplyEyeMakeup/v_ApplyEyeMakeup_g01_c01.avi'))
-frame = y:next_frame()
-print(frame[3]:mean())
-print(frame[1][1][1])
-print(frame[3][y.h][y.w])
-require 'image'
-image.display(frame)
---image.display(frame[2])
---image.display(frame[3])
+if not y:open('/scratch/datasets/ucf101/UCF-101/ApplyEyeMakeup/v_ApplyEyeMakeup_g01_c01.avi') then
+   print("Failed to open video")
+   sys.exit(0)
+end
 
-print("ok")
+frame1 = y:next_frame()
+if math.abs(frame1:mean() - 87.119574652778) > 1e-4 then
+   print("Frame 1 doesn't seem to be correct")
+   sys.exit(0)
+end
+frame2 = y:next_frame()
+if math.abs(frame2:mean() - 87.233272569444) > 1e-4 then
+   print("Frame 2 doesn't seem to be correct")
+   sys.exit(0)
+end
+frame3 = y:next_frame()
+if math.abs(frame3:mean() - 86.159526909722) > 1e-4 then
+   print("Frame 3 doesn't seem to be correct")
+   sys.exit(0)
+end
+
+
+print("Test ok")
